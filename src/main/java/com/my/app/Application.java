1961daaafc6403010815e5b055f0670058664927
ClementCa <clement.carretier@gmail.com>
2017-01-09 17:02:58 +0100

    spring annotation project

--- src/main/java/com/my/app/Application.java
+++ src/main/java/com/my/app/Application.java
@@ -2,13 +2,16 @@ package com.my.app;
 
 import com.my.app.services.CustomerService;
 import com.my.app.services.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by CCA3441 on 09/01/2017.
  */
 public class Application {
     public static void main(String[] args){
-        CustomerService service = new CustomerServiceImpl();
-        System.out.println(service.findAll().get(0).getFirstName());
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CustomerService customerService = applicationContext.getBean("customerService",CustomerService.class);
+        System.out.println(customerService.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/my/app/services/CustomerServiceImpl.java
+++ src/main/java/com/my/app/services/CustomerServiceImpl.java
@@ -3,18 +3,29 @@ package com.my.app.services;
 import com.my.app.model.Customer;
 import com.my.app.repository.CustomerRepository;
 import com.my.app.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    //@Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll();
     }
 
-
+//    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 }
